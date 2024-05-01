60bfd80ab2ccd3eb63d346c478f1037f067a8767
Christopher Barber <christopher.barber@analog.com>
2024-05-01 12:54:20 -0400

    Test default conda format from settings

--- test/cli/test_convert.py
+++ test/cli/test_convert.py
@@ -577,16 +577,12 @@ def test_out_format(test_case: CliTestCaseFactory) -> None:
     wheel_file = test_case.tmp_path.joinpath("fake-1.0.whl")
     wheel_file.write_text("")
 
-    case = test_case(
-        [str(wheel_file)], expected_out_fmt=CondaPackageFormat.V2
-    )
+    case = test_case([str(wheel_file)], expected_out_fmt=CondaPackageFormat.V2)
     case.run()
 
     settings.conda_format = CondaPackageFormat.V1
 
-    case = test_case(
-        [str(wheel_file)], expected_out_fmt=CondaPackageFormat.V1
-    )
+    case = test_case([str(wheel_file)], expected_out_fmt=CondaPackageFormat.V1)
     case.run()
 
     case = test_case(
